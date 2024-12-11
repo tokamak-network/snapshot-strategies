fe5626cef2a6fce82d01d0d550a8e8969703e082
Zena-park <zena@tokamak.network>
2024-12-11 11:35:54 +0900

    feat: applied weights to voting rights

--- src/strategies/tokamak-dao/README.md
+++ src/strategies/tokamak-dao/README.md
@@ -1,21 +1,37 @@
 # Tokamak DAO voting strategy
 
-The voting rights are calculated based on the following assets:
+The voting rights are calculated proportionally based on the following assets:
 
 - TON balance
-
 - WTON balance
-
 - Staked TON balance
-
 - Tokanak NFT balance
-
 - Titan NFT balance
+
+
+The voting rights are calculated using operations and values.
+
+The operations work as follows:
+- except: No voting rights
+- none: Not calculating, Use the balance as is a 1:1 ratio.
+- add: Addition operation
+- minus: Subtraction operation
+- div: Division operation
+- mul: Multiplication operation
+- sqrt: Sqrt operation
+
+
 
 Parameters:
 
-| param |      | sub-param | type | description | default |
+| param |      | sub-param | type | description | example |
 | ---   | ---  | --- | --- | --- | --- |
+| `weight` | `ton` |  `operation`  | `string`|  Weight calculation operator  |  none : No counting to voting
+| `weight` | `ton` |           | `number`| Voting Rights Reflection Ratio    | 0.5
+|          | `wton` |   | `number`| Voting Rights Reflection Ratio  | 0.5
+|          | `swton` |   | `number`| Voting Rights Reflection Ratio  | 1
+|          | `tokamakNft` |   | `number`|  Voting Rights Reflection Ratio  | 1000
+|          | `titanNft` |   | `number`|  Voting Rights Reflection Ratio  | 0
 | `ton` | `name`   |           | `string`|   Strategy name | erc20-balance-of |
 |       | `params` | `address` | `string`|  TON address | 0x2be5e8c109e2197D077D13A82dAead6a9b3433C5 |
 |       |          | `symbol`  | `string`|  TON symbol | TON |
@@ -27,14 +43,43 @@ Parameters:
 | `swton` | `name`   |           | `string`|  Strategy name | staked-ton-balance-of |
 |       | `params` | `address` | `string`|  SeigManager address | 0x0b55a0f463b6defb81c6063973763951712d0e5f |
 |       |          | `symbol`  | `string`|  Staked TON symbol | SWTON |
-| `tokamaknft` | `name`   |           | `string`|  Strategy name | erc721 |
+| `tokamakNft` | `name`   |           | `string`|  Strategy name | erc721 |
 |       | `params` | `address` | `string`|  Tokamak NFT address | 0x073afd0ca0b12da8543e67b664dbf9fcc03fca99 |
 |       |          | `symbol`  | `string`|  Tokamak NFT symbol | TOK |
+| `titanNft` | `name`   |           | `string`|  Strategy name | erc721 |
+|       | `params` | `address` | `string`|  Titan NFT address | 0x0000000000000000000000000000000000000000 |
+|       |          | `symbol`  | `string`|  Titan NFT symbol | TITAN |
+
+
 
 Here is an example of parameters:
 
 ```json
 {
+      "name": "tokamak-dao",
+       "params": {
+        "weight": {
+          "ton": {
+            "operation": "sqrt",
+            "value": 0
+          } ,
+          "wton":  {
+            "operation": "div",
+            "value": 10
+          },
+          "swton":  {
+            "operation": "none",
+            "value": 1
+          },
+          "tokamakNft":  {
+            "operation": "add",
+            "value": 10000
+          },
+          "titanNft":  {
+            "operation": "except",
+            "value": 0
+          }
+        },
         "ton": {
           "name": "erc20-balance-of",
           "params": {
@@ -52,19 +97,28 @@ Here is an example of parameters:
           }
         },
         "swton": {
-          "name": "staked-ton-balance-of",
+          "name": "simple-staking-ton-balance-of",
           "params": {
             "address": "0x0b55a0f463b6defb81c6063973763951712d0e5f",
-            "symbol": "SWTON"
+            "symbol": "SWTON",
+            "decimals": 27
           }
         },
-        "tokamaknft": {
+        "tokamakNft": {
           "name": "erc721",
           "params": {
             "address": "0x073afd0ca0b12da8543e67b664dbf9fcc03fca99",
             "symbol": "TOK"
           }
+        },
+        "titanNft": {
+          "name": "erc721",
+          "params": {
+            "address": "0x0000000000000000000000000000000000000000",
+            "symbol": "TITAN"
+          }
         }
       }
+}
 ```
 
--- src/strategies/tokamak-dao/index.ts
+++ src/strategies/tokamak-dao/index.ts
@@ -1,11 +1,45 @@
 import _strategies from '..';
 import { getAddress } from '@ethersproject/address';
-// import { BigNumberish } from '@ethersproject/bignumber';
-// import { formatUnits } from '@ethersproject/units';
-// import { Multicaller } from '../../utils';
 
 export const author = 'zena';
 export const version = '0.1.0';
+
+const calculate = (a, operation, value) => {
+  let result = a;
+
+  switch (operation) {
+    case 'except': {
+      result = 0;
+      break;
+    }
+    case 'none': {
+      break;
+    }
+    case 'add': {
+      result += value;
+      break;
+    }
+    case 'minus': {
+      result -= value;
+      if (result < 0) result = 0;
+      break;
+    }
+    case 'div': {
+      if (value == 0) result = 0;
+      result /= value;
+      break;
+    }
+    case 'mul': {
+      result *= value;
+      break;
+    }
+    case 'sqrt': {
+      result = Math.sqrt(result);
+      break;
+    }
+  }
+  return result;
+};
 
 export async function strategy(
   space,
@@ -14,6 +48,28 @@ export async function strategy(
   addresses,
   options: {
     name: string;
+    weight: {
+      ton: {
+        operation: string;
+        value: number;
+      };
+      wton: {
+        operation: string;
+        value: number;
+      };
+      swton: {
+        operation: string;
+        value: number;
+      };
+      tokamakNft: {
+        operation: string;
+        value: number;
+      };
+      titanNft: {
+        operation: string;
+        value: number;
+      };
+    };
     ton: {
       name: string;
       params: Record<string, string | number | boolean>;
@@ -26,72 +82,128 @@ export async function strategy(
       name: string;
       params: Record<string, string | number | boolean>;
     };
-    tokamaknft: {
+    tokamakNft: {
+      name: string;
+      params: Record<string, string | number | boolean>;
+    };
+    titanNft: {
       name: string;
       params: Record<string, string | number | boolean>;
     };
   },
   snapshot
 ) {
-  const tonStrategy = _strategies[options.ton.name].strategy;
-  const wtonStrategy = _strategies[options.wton.name].strategy;
-  const swtonStrategy = _strategies[options.swton.name].strategy;
+  let tonVotes, wtonVotes, swtonVotes, tkamakNftVotes, titanNftVotes;
 
-  // const tkamakNftStrategy = _strategies[options.ton.name].strategy;
+  if (options.weight.ton.operation != 'except') {
+    tonVotes = await _strategies[options.ton.name].strategy(
+      space,
+      network,
+      provider,
+      addresses,
+      options.ton.params,
+      snapshot
+    );
+    // console.log('tonVotes', tonVotes);
+  }
 
-  const tonVotes = await tonStrategy(
-    space,
-    network,
-    provider,
-    addresses,
-    options.ton.params,
-    snapshot
-  );
-  // console.log('tonVotes', tonVotes);
+  if (options.weight.wton.operation != 'except') {
+    wtonVotes = await _strategies[options.wton.name].strategy(
+      space,
+      network,
+      provider,
+      addresses,
+      options.wton.params,
+      snapshot
+    );
+    // console.log('wtonVotes', wtonVotes);
+  }
 
-  const wtonVotes = await wtonStrategy(
-    space,
-    network,
-    provider,
-    addresses,
-    options.wton.params,
-    snapshot
-  );
+  if (options.weight.wton.operation != 'except') {
+    swtonVotes = await _strategies[options.swton.name].strategy(
+      space,
+      network,
+      provider,
+      addresses,
+      options.swton.params,
+      snapshot
+    );
+    // console.log('swtonVotes', swtonVotes);
+  }
 
-  // console.log('wtonVotes', wtonVotes);
+  if (options.weight.tokamakNft.operation != 'except') {
+    tkamakNftVotes = await _strategies[options.tokamakNft.name].strategy(
+      space,
+      network,
+      provider,
+      addresses,
+      options.tokamakNft.params,
+      snapshot
+    );
+    // console.log('tkamakNftVotes', tkamakNftVotes);
+  }
 
-  const swtonVotes = await swtonStrategy(
-    space,
-    network,
-    provider,
-    addresses,
-    options.swton.params,
-    snapshot
-  );
+  if (options.weight.titanNft.operation != 'except') {
+    titanNftVotes = await _strategies[options.titanNft.name].strategy(
+      space,
+      network,
+      provider,
+      addresses,
+      options.titanNft.params,
+      snapshot
+    );
+    // console.log('tkamakNftVotes', tkamakNftVotes);
+  }
 
-  // console.log('swtonVotes', swtonVotes);
+  const calculateVotes = (
+    weight,
+    tList,
+    wList,
+    swList,
+    tokamakNft,
+    titanNft,
+    a
+  ) => {
+    let sum = 0;
 
-  // const tkamakNftVotes = await tkamakNftStrategy(
-  //   space,
-  //   network,
-  //   provider,
-  //   addresses,
-  //   options.tokamaknft.params,
-  //   snapshot
-  // );
-  // console.log('tkamakNftVotes', tkamakNftVotes);
+    if (weight.ton != 0 && tList != undefined && tList[a] > 0)
+      sum += calculate(tList[a], weight.ton.operation, weight.ton.value);
 
-  const calculateVotes = (tList, wList, swList, a) => {
-    const tonBalance = tList[a] ? tList[a] : 0;
-    const wtonBalance = wList[a] ? wList[a] : 0;
-    const swtonBalance = swList[a] ? swList[a] : 0;
-    return tonBalance + wtonBalance + swtonBalance;
+    if (weight.wton != 0 && wList != undefined && wList[a] > 0)
+      sum += calculate(wList[a], weight.wton.operation, weight.wton.value);
+
+    if (weight.swton != 0 && swList != undefined && swList[a] > 0)
+      sum += calculate(swList[a], weight.swton.operation, weight.swton.value);
+
+    if (weight.tokamakNft != 0 && tokamakNft != undefined && tokamakNft[a] > 0)
+      sum += calculate(
+        tokamakNft[a],
+        weight.tokamakNft.operation,
+        weight.tokamakNft.value
+      );
+
+    if (weight.titanNft != 0 && titanNft != undefined && titanNft[a] > 0)
+      sum += calculate(
+        titanNft[a],
+        weight.titanNft.operation,
+        weight.titanNft.value
+      );
+
+    return sum;
   };
 
   return Object.fromEntries(
     Object.entries(addresses).map((address: any) => [
       getAddress(address[1]),
-      calculateVotes(tonVotes, wtonVotes, swtonVotes, address[1])
+      calculateVotes(
+        options.weight,
+        tonVotes,
+        wtonVotes,
+        swtonVotes,
+        tkamakNftVotes,
+        titanNftVotes,
+        address[1]
+      )
     ])
   );
 }
